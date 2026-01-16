c643fda7544be73439b582bc5be9f0932e737b41
Edson Brian <edsonbrian1@gmail.com>
2026-01-15 20:34:25 -0500

    avance de dia 1

--- Dia1/primera_clase.py
+++ Dia1/primera_clase.py
@@ -1,1 +1,13 @@
-print("hice nuevos cambios")
+########## print
+print("Hola mundo")
+### strings
+### \ para escapar caracteres especiales
+print("hola \"Edson\"")
+### \n nueva linea
+print("primera linea\nsegunda linea")
+### \t tabular texto
+print("\tlinea tabulada")
+
+### Funcion Input
+input("Escribe tu nombre: ")
+
---
+++ Dia1/proyecto1/proyecto1.py
@@ -0,0 +1,5 @@
+"""
+Este proyecto tiene que pedir dos nombres al usuario
+y con ambos nombre crear el nombre de una cerverza
+"""
+print("El nombre de la cerveza es\n\""+ input("Dame el primer nombre: ") + input("Dame el segundo nombre: ")+"\"")
